113c57f02a945e97a239b95115a0779a9fa1ac83
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2024-02-07 18:50:40 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    let paragraph = document.getElementById("my-paragraph");
+    paragraph.innerHTML = "This paragraph was updated!";
+
+    let image = document.getElementById("image");
+    image.style.width = "300px";
+}
